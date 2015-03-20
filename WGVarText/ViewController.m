d191d05f715e3982a405d5891782c2d712f9cbf8
wanggang <wgang316@163.com>
2015-03-20 10:44:40 +0800

    Initial Commit

---
+++ WGVarText/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WGVarText
+//
+//  Created by 王刚 on 15/3/20.
+//  Copyright (c) 2015年 wwwlife. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
